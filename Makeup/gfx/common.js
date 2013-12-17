caec679ba69fbdfa5cd703e5d2c2956e2c5e64b6
Evgeny E. Neverov <help@assist2.me>
2013-12-17 15:59:52 +0900

    Player start

--- Makeup/gfx/common.js
+++ Makeup/gfx/common.js
@@ -1,7 +1,40 @@
 $(function() {
-	$('.elka').mouseenter(function() {
-		$(this).addClass('elka-frozen');
-	}).mouseleave(function() {
-		$(this).removeClass('elka-frozen');
+	var isChrome = navigator.userAgent.indexOf('Chrome') !== -1,
+		players = [];
+
+	var Player = function(color) {
+		this.x = this.y = 0;
+		this.speed = 100;
+		this.color = color || 'yellow';
+
+		this.build = function() {
+			$('body').append('<div class="elka elka-' + this.color + ' elka-normal"><i class="i0"></i><i class="i1"></i><i class="i2"></i><i class="i3"></i><i class="i4"></i><i class="i5"></i></div>');
+		};
+
+		this.move = function(e) {
+			switch(e.which) {
+				case 37: // left
+					
+					break;
+				case 38: // top
+					
+					break;
+				case 39: // right
+					
+					break;
+				case 40: // down
+					
+					break;
+			}
+		}
+
+		this.build();
+		$(document).on('keydown', this.move);
+	}
+
+	$(document).on('mouseenter mouseleave', '.elka', function(e) {
+		$(this)[e.type == 'mouseenter' ? 'addClass' : 'removeClass']('elka-frozen');
 	});
+
+	players[0] = new Player();
 })
