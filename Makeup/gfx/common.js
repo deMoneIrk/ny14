fa3df6c07888865cc48ef0455cbabdaeed39f2fb
Evgeny E. Neverov <help@assist2.me>
2013-12-18 17:39:38 +0900

    Speed set

--- Makeup/gfx/common.js
+++ Makeup/gfx/common.js
@@ -3,33 +3,56 @@ $(function() {
 		players = [];
 
 	var Player = function(color) {
-		this.x = this.y = 0;
-		this.speed = 100;
+		this.moveValue = 0.025;
+
+		this.pos = [0, 0];
+		this.max = [2, 2];
+		this.speed = [0, 0];
+		this.moving = [0, 0];
+
 		this.color = color || 'yellow';
 
-		this.build = function() {
-			$('body').append('<div class="elka elka-' + this.color + ' elka-normal"><i class="i0"></i><i class="i1"></i><i class="i2"></i><i class="i3"></i><i class="i4"></i><i class="i5"></i></div>');
-		};
-
-		this.move = function(e) {
-			switch(e.which) {
-				case 37: // left
-					
-					break;
-				case 38: // top
-					
-					break;
-				case 39: // right
-					
-					break;
-				case 40: // down
-					
-					break;
-			}
-		}
-
 		this.build();
-		$(document).on('keydown', this.move);
+
+		var $this = this;
+		$(document).on('keydown keyup', function(e) {
+			$this.key(e);
+		});
+
+		setInterval(function() {
+			$this.tick();
+		}, 100);
+	}
+
+	var proto = Player.prototype;
+
+	proto.build = function() {
+		$('body').append('<div class="elka elka-' + this.color + ' elka-normal"><i class="i0"></i><i class="i1"></i><i class="i2"></i><i class="i3"></i><i class="i4"></i><i class="i5"></i></div>');
+	}
+
+	proto.key = function(e) {
+		switch(e.which) {
+			case 37: this.moving[0] = e.type == 'keydown' ? -this.moveValue : 0; break;
+			case 38: this.moving[1] = e.type == 'keydown' ? +this.moveValue : 0; break;
+			case 39: this.moving[0] = e.type == 'keydown' ? +this.moveValue : 0; break;
+			case 40: this.moving[1] = e.type == 'keydown' ? -this.moveValue : 0; break;
+		}
+	}
+
+	proto.tick = function() {
+		// Устанавливаем текущую скорость
+		this.speed[0] += this.moving[0];
+		if (!this.moving[0] && this.speed[0]) this.speed[0] += this.speed[0] > 0 ? -this.moveValue : this.moveValue;
+		if (this.speed[0] > this.max[0]) this.speed[0] = this.max[0]; if (this.speed[0] < -this.max[0]) this.speed[0] = -this.max[0];
+
+		this.speed[1] += this.moving[1];
+		if (!this.moving[1] && this.speed[1]) this.speed[1] += this.speed[1] > 0 ? -this.moveValue : this.moveValue;
+		if (this.speed[1] > this.max[1]) this.speed[1] = this.max[1]; if (this.speed[1] < -this.max[1]) this.speed[1] = -this.max[1];
+
+		this.speed[0] = parseFloat(this.speed[0].toFixed(3));
+		this.speed[1] = parseFloat(this.speed[1].toFixed(3));
+
+		
 	}
 
 	$(document).on('mouseenter mouseleave', '.elka', function(e) {
@@ -37,4 +60,4 @@ $(function() {
 	});
 
 	players[0] = new Player();
-})
+});
