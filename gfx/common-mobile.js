787f1f607748fb13be10cc4cfb1f00155976c2aa
Evgeny E. Neverov <evgeny@neverov.name>
2013-12-26 03:52:51 +0900

    Joystick restrictions

--- gfx/common-mobile.js
+++ gfx/common-mobile.js
@@ -32,7 +32,7 @@ socket.on('system', function(data) {
 });
 
 gameStart = function() {
-	var wh = [parseInt($('.stick i').width()) / 2, parseInt($('.stick i').height()) / 2], pos = [$('.stick i').position().left + wh[0], $('.stick i').position().top + wh[1]];
+	var wh = [parseInt($('.stick i').width()) / 2, parseInt($('.stick i').height()) / 2], pos = [$('.stick i').position().left + wh[0], $('.stick i').position().top + wh[1]], max = 140;
 	socket.emit('iphone', { wh: wh, pos: pos });
 
 	$('body').addClass('fade_second_screen');
@@ -44,6 +44,7 @@ gameStart = function() {
 
 	$('.strike').on('touchstart', function(e) {
 		socket.emit('iphone');
+		top.location.reload();
 	});
 
 	var start = []
@@ -53,7 +54,29 @@ gameStart = function() {
 	}).on('touchmove', function(e) {
 		var now = [e.originalEvent.targetTouches[0].screenX, e.originalEvent.targetTouches[0].screenY],
 			change = [start[0] - now[0], start[1] - now[1]];
-		socket.emit('iphone', { now: now, change: change });
+
+		if (change[0] > max) change[0] = max; if (change[0] < -max) change[0] = -max;
+		if (change[1] > max) change[1] = max; if (change[1] < -max) change[1] = -max;
+
+		change[2] = Math.sqrt(Math.pow(change[0], 2) + Math.pow(change[1], 2) - 2 * change[0] * change[1] * Math.cos(90 * Math.PI / 180));
+		var
+			angle1 = Math.acos((Math.pow(change[0], 2) + Math.pow(change[2], 2) - Math.pow(change[1], 2)) / (2 * change[0] * change[2])) * 180 / Math.PI,
+			angle0 = 90 - angle0,
+			angle2 = 90;
+
+		if (change[2] < -max) {
+			change[2] = -max;
+			change[1] = change[2] * (Math.sin(angle0 * Math.PI / 180) / Math.sin(angle1 * Math.PI / 180));
+			change[0] = change[2] * (Math.sin(angle1 * Math.PI / 180) / Math.sin(angle0 * Math.PI / 180));
+		} else if (change[2] > max) {
+			change[2] = max;
+			change[1] = change[2] * (Math.sin(angle0 * Math.PI / 180) / Math.sin(angle1 * Math.PI / 180));
+			change[0] = change[2] * (Math.sin(angle1 * Math.PI / 180) / Math.sin(angle0 * Math.PI / 180));
+		}
+
+		$(this).css('transform', 'translate' + (Modernizr.csstransforms3d ? '3d' : '') + '(' + (-change[0]) + 'px, ' + (-change[1]) + 'px' + (Modernizr.csstransforms3d ? ', 0px' : '') + ')');
+
+		socket.emit('iphone', { now: now, change: change, angle: angle });
 	});
 };
 
@@ -62,7 +85,9 @@ gameStop = function() {
 };
 
 $(function() {
-	gameStart();
+	setTimeout(function() {
+		gameStart();
+	}, 500);
 
 	if(window.orientation == -90 || window.orientation == 90 || window.orientation == undefined) {
 		$('body').addClass('horizontal');
